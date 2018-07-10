bcaadc0424c5d48147cc1ab5c752a8977de8e43d
Benecia Shi <Benecia@shannons-macbook-air.local>
2018-07-10 10:31:53 -0500

    Set up Storyboard

--- Top Stories/SourcesViewController.swift
+++ Top Stories/SourcesViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SourcesViewController: UIViewController {
+class SourcesViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
